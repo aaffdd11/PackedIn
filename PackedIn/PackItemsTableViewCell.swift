463e3373031a5c577ddbcb13cffe3a7c51424db1
ChenYuan <Welkin@ChenYuans-iMac.local>
2015-03-01 13:57:03 +0800

    using custom tableviewcell. gesture is disabled so that in the controller, actions will be triggered.

--- PackedIn/PackItemsTableViewCell.swift
+++ PackedIn/PackItemsTableViewCell.swift
@@ -21,10 +21,10 @@ class PackItemsTableViewCell: UITableViewCell {
         super.init(style: style, reuseIdentifier: reuseIdentifier)
         self.selectionStyle = .None
         self.backgroundColor = UIColor(white: 1.0, alpha: 0.7)
-        
-        var recognizer = UIPanGestureRecognizer(target: self, action: "handleStats:")
-        recognizer.delegate = self
-        addGestureRecognizer(recognizer)
+//        
+//        var recognizer = UIPanGestureRecognizer(target: self, action: "handleStats:")
+//        recognizer.delegate = self
+//        addGestureRecognizer(recognizer)
     }
     
     override func awakeFromNib() {
@@ -37,41 +37,41 @@ class PackItemsTableViewCell: UITableViewCell {
 
         // Configure the view for the selected state
     }
-
-    //MARK: - horizontal pan gesture methods
-    func handleStats(recognizer: UIPanGestureRecognizer) {
-        // 1
-        if recognizer.state == .Began {
-            // when the gesture begins, record the current center location
-            originalCenter = center
-        }
-        // 2
-        if recognizer.state == .Changed {
-            let translation = recognizer.translationInView(self)
-            center = CGPointMake(originalCenter.x + translation.x, originalCenter.y)
-            // has the user dragged the item far enough to initiate a delete/complete?
-            deleteOnDragRelease = frame.origin.x < -frame.size.width / 2.0
-        }
-        // 3
-        if recognizer.state == .Ended {
-            // the frame this cell had before user dragged it
-            let originalFrame = CGRect(x: 0, y: frame.origin.y,
-                width: bounds.size.width, height: bounds.size.height)
-            if !deleteOnDragRelease {
-                // if the item is not being deleted, snap back to the original location
-                UIView.animateWithDuration(0.2, animations: {self.frame = originalFrame})
-            }
-        }
-    }
-    
-    override func gestureRecognizerShouldBegin(gestureRecognizer: UIGestureRecognizer) -> Bool {
-        if let panGestureRecognizer = gestureRecognizer as? UIPanGestureRecognizer {
-            let translation = panGestureRecognizer.translationInView(superview!)
-            if fabs(translation.x) > fabs(translation.y) {
-                return true
-            }
-            return false
-        }
-        return false
-    }
+//
+//    //MARK: - horizontal pan gesture methods
+//    func handleStats(recognizer: UIPanGestureRecognizer) {
+//        // 1
+//        if recognizer.state == .Began {
+//            // when the gesture begins, record the current center location
+//            originalCenter = center
+//        }
+//        // 2
+//        if recognizer.state == .Changed {
+//            let translation = recognizer.translationInView(self)
+//            center = CGPointMake(originalCenter.x + translation.x, originalCenter.y)
+//            // has the user dragged the item far enough to initiate a delete/complete?
+//            deleteOnDragRelease = frame.origin.x < -frame.size.width / 2.0
+//        }
+//        // 3
+//        if recognizer.state == .Ended {
+//            // the frame this cell had before user dragged it
+//            let originalFrame = CGRect(x: 0, y: frame.origin.y,
+//                width: bounds.size.width, height: bounds.size.height)
+//            if !deleteOnDragRelease {
+//                // if the item is not being deleted, snap back to the original location
+//                UIView.animateWithDuration(0.2, animations: {self.frame = originalFrame})
+//            }
+//        }
+//    }
+//    
+//    override func gestureRecognizerShouldBegin(gestureRecognizer: UIGestureRecognizer) -> Bool {
+//        if let panGestureRecognizer = gestureRecognizer as? UIPanGestureRecognizer {
+//            let translation = panGestureRecognizer.translationInView(superview!)
+//            if fabs(translation.x) > fabs(translation.y) {
+//                return true
+//            }
+//            return false
+//        }
+//        return false
+//    }
 }
